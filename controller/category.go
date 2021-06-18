58a674f88d5271b0172d9dba4a9c81b52b50da73
Navas <gabrielnavas@email.com>
2021-06-17 22:38:31 -0300

    add function get ip

--- controller/category.go
+++ controller/category.go
@@ -5,8 +5,11 @@ import (
 	servicecategory "books_api/service"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
+	"net"
 	"net/http"
+	"strings"
 )
 
 type CategoryController interface {
@@ -28,7 +31,12 @@ func NewCategoryRepository(categoryService servicecategory.InsertOneCategory) *c
 func (c *categoryControllerImpl) Create(w http.ResponseWriter, r *http.Request) {
 	category := entity.Category{}
 
-	err := json.NewDecoder(r.Body).Decode(&category)
+	_, err := getIP(r)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&category)
 	if err != nil && err == io.EOF {
 		err = errors.New("no data on body found")
 		responseJsonErr(&w, err, http.StatusBadRequest)
@@ -37,7 +45,7 @@ func (c *categoryControllerImpl) Create(w http.ResponseWriter, r *http.Request) 
 
 	category_created, err := c.categoryService.InsertOne(category)
 	if err != nil {
-		if err == service.ErrRepositoryError {
+		if err == servicecategory.ErrRepositoryError {
 			responseJsonErr(&w, err, http.StatusInternalServerError)
 			return
 		}
@@ -46,4 +54,37 @@ func (c *categoryControllerImpl) Create(w http.ResponseWriter, r *http.Request) 
 	}
 
 	responseCreated(&w, category_created)
+}
+
+func getIP(r *http.Request) (string, error) {
+	//Get IP from the X-REAL-IP header
+	ip := r.Header.Get("X-REAL-IP")
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		fmt.Printf("X-REAL-IP: %v\n", ip)
+		return ip, nil
+	}
+
+	//Get IP from X-FORWARDED-FOR header
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	splitIps := strings.Split(ips, ",")
+	for _, ip := range splitIps {
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			fmt.Printf("X-REAL-IP: %v\n", ip)
+			return ip, nil
+		}
+	}
+
+	//Get IP from RemoteAddr
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	netIP = net.ParseIP(ip)
+	if netIP != nil {
+		fmt.Printf("X-REAL-IP: %v\n", ip)
+		return ip, nil
+	}
+	return "", fmt.Errorf("no valid ip found")
 }
